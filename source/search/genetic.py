2c121e948b3ebeb304b127f79d59d45af88facea
Marina <ivanovicmimi@gmail.com>
2020-04-17 01:44:28 +0200

    genetic regular

--- source/search/genetic.py
+++ source/search/genetic.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from .searcher import Searcher
-from ..utils.geneticmagic import GeneticMagicSquare
+from ..utils.magic import MagicSquare
 
 class GeneticAlgorithm(Searcher):
 
@@ -23,11 +23,11 @@ class GeneticAlgorithm(Searcher):
         total_violation_sum = 0
 
         for _ in range(self.population_num):
-            gms = GeneticMagicSquare(self.n)
-            gms.init_random()
-            self.population.append(gms)
+            ms = MagicSquare(self.n)
+            ms.init_random()
+            self.population.append(ms)
 
-            total_violation_sum += gms.violation_number()
+            total_violation_sum += ms.violation_number()
 
         self._set_best_magic_square()
         self.iter = iterations
@@ -45,19 +45,6 @@ class GeneticAlgorithm(Searcher):
             total_violation_sum += ms.violation_number()
         
         self.mean_violations.append(total_violation_sum / self.population_num)
-
-    def _chromosome_to_square(self, chromosome):
-        pos = np.zeros(shape=chromosome.shape, dtype=int)
-        for i in range(len(chromosome)-1, -1, -1):
-            pos[i] = chromosome[i]
-            for j in range(i + 1, len(chromosome)):
-                if pos[j] >= pos[i]:
-                    pos[j] += 1
-
-        square = np.zeros(shape=chromosome.shape, dtype=int)
-        for i in range(len(chromosome)):
-            square[pos[i]] = i + 1
-        return square.reshape((self.magic_square['n'], -1))
 
     def __getitem__(self, key):
         if key == 'mean viol. through iter.':
