b4184a5063da76d9a6d9d7dcfbd1926fe1d90658
Marina <ivanovicmimi@gmail.com>
2020-04-17 19:43:42 +0200

    genetic algoritham in a nutshell

--- source/search/genetic.py
+++ source/search/genetic.py
@@ -7,39 +7,41 @@ from ..utils.magic import MagicSquare
 
 class GeneticAlgorithm(Searcher):
 
-    def __init__(self, magic_square, succ_num=30, population_num=4):
+    def __init__(self, magic_square, succ_num=30, population_size=100):
         super(GeneticAlgorithm, self).__init__(magic_square, succ_num)
         self.type = 'Genetic Algorithm'
-        self.population_num = population_num
-        self.elite_size = 1
+        self.population_size = population_size
+        self.elite_size = int(0.2 * population_size)
         self.mutation_rate = 0.1
         self.mean_violations = None
 
     def find(self, iterations):
         self._init_start_state(iterations)
-        # TODO
 
-    def _crossover_population(self, mating_pool):
-        mating_pool = self._get_mating_pool()
-        children = []
-        for i in range(self.elite_size):
-            children.append(mating_pool[i])
+        for it in range(iterations):
+            new_population = self._get_elite()
+            for _ in range(self.population_size - self.elite_size):
+                parent_a, parent_b = self._chose_parents()
+                child = self._crossover(parent_a, parent_a)
+                child = self._mutate(child)
+                new_population.append(child)
+            self.population = new_population
+
+            self._set_best_magic_square()
+            if self._should_break(it):
+                break
+
+    def _get_elite(self):
+        sorted(self.population, key=self._fitness)
+        new_population = []
         
-        # TODO: breed by probability
-        mating_pool = random.sample(mating_pool, len(mating_pool))
-        for i in range(len(mating_pool) - self.elite_size):
-            child = self._crossover(
-                mating_pool[i],
-                mating_pool[len(mating_pool) - i - 1]
-            )
-            children.append(child)
-        return children
+        for individual in self.population[0 : self.elite_size]:
+            new_population.append(individual)
+        return new_population
 
-    def _get_mating_pool(self):
+    def _chose_parents(self):
         # TODO: by probability
-        mating_pool = self.population
-        mating_pool = random.sample(mating_pool, len(mating_pool))
-        return sorted(mating_pool, key=self._fitness)
+        return self.population[0], self.population[1]
 
     def _crossover(self, parent_a, parent_b):
         parent_a = parent_a['matrix'].reshape((1, -1))[0]
@@ -62,14 +64,6 @@ class GeneticAlgorithm(Searcher):
         child_ms.set_matrix(child)
         return child_ms
 
-    def _mutate_population(self):
-        nonmutated_population = copy.deepcopy(self.population)
-        self.population = []
-        
-        for individual in nonmutated_population:
-            mutated_individual = self._mutate(individual)
-            self.population.append(mutated_individual)
-
     def _mutate(self, individual):
         mutated_individual = copy.deepcopy(individual)
         if random.random() < self.mutation_rate:
@@ -87,7 +81,7 @@ class GeneticAlgorithm(Searcher):
         self.mean_violations = []
         total_violation_sum = 0
 
-        for _ in range(self.population_num):
+        for _ in range(self.population_size):
             ms = MagicSquare(self.n)
             ms.init_random()
             self.population.append(ms)
@@ -109,7 +103,7 @@ class GeneticAlgorithm(Searcher):
                 self.magic_square = ms
             total_violation_sum += ms.violation_number()
         
-        self.mean_violations.append(total_violation_sum / self.population_num)
+        self.mean_violations.append(total_violation_sum / self.population_size)
 
     def __getitem__(self, key):
         if key == 'mean viol. through iter.':
